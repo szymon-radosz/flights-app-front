5a17e71e0940d39c7185390a9854235f1cd703f2
Simon <radoszszymon@gmail.com>
2023-03-08 13:53:59 +0100

    fix: firebase configuration for analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,18 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 import '../styles/tailwind.css';
 import '../styles/slick.css';
 
-import analytics from '../../firebase';
+// import {analytics} from '../../firebase';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  useEffect(() => {
-    if (process?.env?.NODE_ENV === 'production') {
-      analytics();
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (process?.env?.NODE_ENV === 'production') {
+  //     analytics();
+  //   }
+  // }, []);
 
   return <Component key={router.asPath} {...pageProps} />;
 }
